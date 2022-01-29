ce18d0ca5f4835bd262c236a98287e26640469eb
TCMKURAL <m-kural@hotmail.com>
2022-01-30 01:06:27 +0300

    creating_api

---
+++ src/utils/api.js
@@ -0,0 +1,26 @@
+import { _getQuestions } from "./_DATA";
+import { _getUsers } from "./_DATA";
+import { _saveQuestion } from "./_DATA";
+import { _saveQuestionAnswer } from "./_DATA";
+
+export function getInitialData(){
+    return Promise.all([_getUsers(), _getQuestions()]).then(
+        ([users, questions]) =>({
+            users,
+            questions
+        })
+    )
+}
+
+export function saveQuestion(){
+    return Promise.all([_saveQuestion]).then(
+        ([question]) =>{
+            question
+        }
+    )
+}
+
+export function saveQuestionAnswer(answer){
+    return _saveQuestionAnswer(answer)
+    
+}
