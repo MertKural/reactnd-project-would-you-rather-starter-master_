9d8084b41bc89852ecdad548c21d818bcbb1a4f3
TCMKURAL <m-kural@hotmail.com>
2022-01-30 12:02:58 +0300

    middleware&remaining_reducers

---
+++ src/middleware/logger.js
@@ -0,0 +1,8 @@
+const logger = (store) => (next) => (action) => {
+    console.group(action.type);
+    console.log("The action: ", action)
+    const returnVal = next(action)
+    console.log("The new state: ", store.getState())
+    console.groupEnd
+    return returnVal
+} 
