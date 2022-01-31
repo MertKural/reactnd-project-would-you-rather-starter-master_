4e5b6bbef90885ae42860bb180be2bf8fe42d463
TCMKURAL <m-kural@hotmail.com>
2022-01-31 19:42:04 +0300

    refactor_project_structure&add_dependincies

---
+++ src/components/QuestionList.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {Component} from 'react'
+
+class QuestionList extends React.Component{
+    render(){
+        return(
+            <></>
+        )
+    }
+}
+
+export default QuestionList 
---
+++ src/components/Questions.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {Component} from 'react'
+
+class Questions extends React.Component{
+    render(){
+        return(
+            <></>
+        )
+    }
+}
+
+export default Questions 
